c8bc021c4ed214069a661eba3c766766429971db
Mathew Cortez <mcortez121@gmail.com>
2016-06-24 09:49:53 -0700

    Minified files and compressed images
    I compressed all of the JavaScript files, inlined the CSS file, and
    compressed the images used in the game to help improve performance.

--- README.md
+++ README.md
@@ -8,15 +8,17 @@ For detailed instructions on how to get started, check out this [guide](https://
 ###About the game
 This game is a replication of the classical arcade game Frogger with a few different features.  
 ###Description
-Your goal is to avoid the bugs on your way to reaching the water.  If you collide with one of the bugs you will be reset back to your starting location for you to try again.
+Your goal is to avoid the bugs on your way to reaching the water.  If you collide with one of the bugs you will be reset back to your starting location for you to try again and you lose a life.  You have a max of three lives.
 Each time you reach the water you will receive a point.
 ###Enemies
 The bugs are the main.  The bugs will scurry past the screen at varying speeds.
 ###How to start playing the game
 Download the game files or fork them from github [Link](https://github.com
 Open index.html in your browser
-The game will start with your character in position and ready to play
+The game will start with your character in position and ready to play.  
 ###Game Mechanics 
 Moving is the only mechanic and is done by using the arrows (left, right, up, and down)
 ###Restarting the game
 To restart the game just press the refresh page button or press F5
+###Future Work
+I need to figure out how to display and take awya hearts when you lose them in the game.
--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+'use strict';
 // Enemies our player must avoid
+// got some help from the Udacity discussion board as well as some inspiration from the share your project thread.
 var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -20,18 +22,15 @@ Enemy.prototype.update = function(dt) {
     this.x = this.x + 101 * this.speed * dt;
     //collision detection
     if (this.y == player.y - 18 && (this.x > player.x - 70 && this.x < player.x + 70)) {
-        if (player.lives > 1) {
+        if (player.lives >= 1) {
             player.lives -= 1;
             player.reset();
-        }
-        else {
-            player.gameOver();
         }
     }
     
     // when the bug goes off the screen move it back to the other side
     if (this.x > 505) {
-        this.reset();    
+        this.reset();   
     }
 };
 
@@ -53,14 +52,18 @@ Enemy.prototype.render = function() {
 var Player = function(x, y) {
     this.x = x;
     this.y = y;
-    this.score = 0;
+    this.score = 0; // setting the lives and score of the player
     this.lives = 3;
-    this.sprite = 'images/char-boy.png'
+    this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function(dt) {
     this.x = this.x;
     this.y = this.y;
+    if (this.y < 50) {  // when player crosses the victory line
+        this.reset();
+        this.updateScore(1); // add one to the score
+    }
 };
 
 Player.prototype.render = function() {
@@ -69,46 +72,43 @@ Player.prototype.render = function() {
 
 Player.prototype.reset = function() {
     this.x = 202;
-    this.y = 404;
-}
+    this.y = 404;// starting location
+};
 
-Player.prototype.handleInput = function(key) {
-    if (this.y > 0 && key === "up") {
-        this.y -= 83;
+Player.prototype.handleInput = function(key) { // moving
+    if (this.lives !== 0) {
+        if (this.y > 0 && key === "up") {
+            this.y -= 83;
+        }
+        if (this.y < 404 && key === "down") {
+            this.y += 83;
+        }
+        if (this.x > 0 && key === "left") {
+            this.x -= 101;
+        }
+        if (this.x < 404 && key === "right") {
+            this.x += 101;
+       }
     }
-    if (this.y < 404 && key === "down") {
-        this.y += 83;
-    }
-    if (this.x > 0 && key === "left") {
-        this.x -= 101;
-    }
-    if (this.x < 404 && key === "right") {
-        this.x += 101;
-    }
-    if (this.y < 50) {
-        this.reset();
-        this.updateScore();
+    else {
+        this.gameOver();
     }
 };
 
-Player.prototype.updateScore = function(score) {
-    this.score += score;
-    $('#score').text(this.score);
+Player.prototype.updateScore = function (score) {
+    this.score += score;  // increment score
+    document.getElementById("score").innerHTML = "Score: " + this.score; // change the score on the html side
 };
 
+
+
+Player.prototype.gameOver = function () {
+  document.getElementById("score").innerHTML = "Final Score: " + this.score + "<br> Refresh to restart the game";
+};
+    
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 
-/*var allEnemies = [
-    new Enemy(-100, 59, 100),
-    new Enemy(-100, 145, 150),
-    new Enemy(-100, 230, 200)
-];
-
-for (var i = 0; i < 5; i++) {
-    var enemy = new Enemy (-100, (i + 1) * 83);
-    allEnemies.push(enemy);
-}*/
 
 var allEnemies = [];
 var enemyRow = [54, 137, 220];
@@ -119,6 +119,8 @@ for (var i = 0; i < 5; i++) {
     var enemy = new Enemy (x, y);
     allEnemies.push(enemy);
 }
+
+
 
 // Place the player object in a variable called player
 //player starting location
